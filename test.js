1f7e9fcfbdc83dd04f2b7887a4b7bfd645856418
MindSmith53 <work.areeb01@gmail.com>
2025-07-22 14:53:29 +0530

    dom

--- test.js
+++ test.js
@@ -7,6 +7,28 @@ let my2 = myArr.splice(1, 3);
 console.log(my2);
 console.log("C ", myArr);
 
+// var message = prompt("Enter your message", "");
+// var counter = parseInt(prompt("How many Times?", "10"));
 
+// if (!message || isNaN(counter) || counter <= 0) {
+//   alert("Please enter a valid message and number.");
+//   return;
+// }
 
+// window.InputEvent = window.Event || window.InputEvent;
+// var event = new InputEvent("input", { bubbles: true });
+// var textbox = document.querySelector("div[contenteditable][role='textbox']");
+// var sendButton = document.querySelector("button[aria-label='Send']");
 
+// if (!textbox || !sendButton) {
+//   alert("Could not find textbox or send button.");
+//   return;
+// }
+
+// for (let index = 0; index < counter; index++) {
+//   setTimeout(() => {
+//     textbox.value = message;
+//     textbox.dispatchEvent(event);
+//     sendButton.click();
+//   }, index * 1000);
+// }
